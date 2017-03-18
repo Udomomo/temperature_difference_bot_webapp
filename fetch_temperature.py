22b1ac6346b77a286493a0d8e81a52383b9f6c06
Udomomo <batzuma@yahoo.co.jp>
2017-03-18 08:10:15 +0000

    make tweet feature

--- fetch_temperature.py
+++ fetch_temperature.py
@@ -2,29 +2,36 @@ from forecastiopy import *
 
 
 #fetch Tokyo's weather forecast from Dark Sky
-apikey = "08c19e7bfc9504e9c4f8db40bf1d9605"
+def fetch_forecast_data():
+    apikey = "08c19e7bfc9504e9c4f8db40bf1d9605"
+    Tokyo = [35.689781, 139.762202]
 
-Tokyo = [35.689781, 139.762202]
-
-fio = ForecastIO.ForecastIO(apikey,
+    forecast_data = ForecastIO.ForecastIO(apikey,
                             units=ForecastIO.ForecastIO.UNITS_SI,
                             lang=ForecastIO.ForecastIO.LANG_ENGLISH,
                             latitude=Tokyo[0], longitude=Tokyo[1])
 
-print('Latitude', fio.latitude, 'Longitude', fio.longitude)
-print('Timezone', fio.timezone, 'Offset', fio.offset)
-print(fio.get_url()) # You might want to see the request url
+    '''for debug
+    print('Latitude', forecast_data.latitude, 'Lengitude', forecast_data.longitude)
+    print('Timezone', forecast_data.timezone, 'Offset', forecast_data.offset)
+    print(forecast_data.get_url()) # You might want to see the request url
+    '''
+    return forecast_data
 
 #extract the temperature from the data
-temperatureData=[]
-if fio.has_daily() is True:
-    daily = FIODaily.FIODaily(fio)
-    outputKey = ["time", "temperatureMax", "temperatureMin"]
-    for day in range(2):
-        dailyTemperature = {}
-        for item in outputKey:
-            dailyTemperature[item] = str(daily.get_day(day+1)[item]) # daily.get_day(day) fetches ['daily'] ['data'][day-1]
-        temperatureData.append(dailyTemperature)
-    print(temperatureData)
-else:
-    print('No Daily data')
+def extract_temperature():
+    fio = fetch_forecast_data()
+    temperatureData=[]
+    if fio.has_daily() is True:
+        daily = FIODaily.FIODaily(fio)
+        outputKey = ["time", "temperatureMax", "temperatureMin"]
+        for day in range(2):
+            dailyTemperature = {}
+            for item in outputKey:
+                dailyTemperature[item] = str(daily.get_day(day+1)[item]) # daily.get_day(day) fetches ['daily'] ['data'][day-1]
+            temperatureData.append(dailyTemperature)
+        return(temperatureData)
+    else:
+        return('No Daily data')
+
+
---
+++ tweet.py
@@ -0,0 +1,68 @@
+# -*- coding: utf-8 -*-
+
+import os
+import sys
+from datetime import datetime
+import tweepy
+from decimal import *
+from fetch_temperature import extract_temperature
+
+#validate data existence
+#data = extract_temperature()
+
+data = [{'temperatureMin': '5.2', 'time': '1489762800', 'temperatureMax': '14.89'}, {'temperatureMin': '4.39', 'time': '1489849200', 'temperatureMax': '17.89'}]
+
+if data == "No Daily data":
+    sys.exit() 
+
+print(data)
+
+#set temperature difference base-line
+difference_line = 3
+
+#set tweet sentence
+today_temperatureMax = Decimal(data[0]["temperatureMax"])
+tomorrow_temperatureMax = Decimal(data[1]["temperatureMax"])
+today_temperatureMin = Decimal(data[0]["temperatureMin"])        
+tomorrow_temperatureMin = Decimal(data[1]["temperatureMin"])
+temperatureMax_difference = today_temperatureMax - tomorrow_temperatureMax
+temperatureMin_difference = today_temperatureMin - tomorrow_temperatureMin
+
+def plus_digit(number):
+    if number > 0:
+        return "+"
+    else:
+        return ""
+
+tweet = "・明日の最高気温：" + str(tomorrow_temperatureMax) + "℃\n　（前日比" + plus_digit(temperatureMax_difference) + str(temperatureMax_difference) + "℃）\n・明日の最低気温：" + str(tomorrow_temperatureMin) + "℃\n　（前日比" + plus_digit(temperatureMin_difference) + str(temperatureMin_difference) + "℃）\n寒暖差が大きいです。体調に気をつけましょう。"
+
+#set API key
+consumer_key = os.environ["CONSUMER_KEY_TEMPBOT"]
+consumer_secret = os.environ["CONSUMER_SECRET_TEMPBOT"]
+access_token = os.environ["ACCESS_TOKEN_KEY_TEMPBOT"]
+access_token_secret = os.environ["ACCESS_TOKEN_SECRET_TEMPBOT"]
+
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+api = tweepy.API(auth_handler=auth)
+
+
+#judge to tweet or not
+def judge_difference():
+    if abs(temperatureMax_difference) >= difference_line  or abs(temperatureMin_difference) >= difference_line:
+        return True
+    else:
+        return False
+
+print(judge_difference())
+
+#execute tweet
+def make_tweet():
+    if judge_difference() == True:
+        print(tweet)      
+        api.update_status(tweet)        
+    else:
+        sys.exit()
+
+make_tweet()
+
