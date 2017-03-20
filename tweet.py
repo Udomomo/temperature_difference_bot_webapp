9519ee7015a0574568f4f0aafa881c7f10fb2792
Udomomo <batzuma@yahoo.co.jp>
2017-03-20 13:10:39 +0000

    modified temperature difference calculation

--- tweet.py
+++ tweet.py
@@ -7,7 +7,7 @@ import tweepy
 from decimal import *
 from fetch_temperature import extract_temperature
 
-#validate data existence
+#check if temperature data exists
 data = extract_temperature()
 
 if data == "No Daily data":
@@ -15,7 +15,7 @@ if data == "No Daily data":
 
 print(data)
 
-#set temperature difference base-line
+#set temperature difference baseline
 difference_line = 3
 
 #set tweet sentence
@@ -23,16 +23,24 @@ today_temperatureMax = Decimal(data[0]["temperatureMax"])
 tomorrow_temperatureMax = Decimal(data[1]["temperatureMax"])
 today_temperatureMin = Decimal(data[0]["temperatureMin"])        
 tomorrow_temperatureMin = Decimal(data[1]["temperatureMin"])
-temperatureMax_difference = today_temperatureMax - tomorrow_temperatureMax
-temperatureMin_difference = today_temperatureMin - tomorrow_temperatureMin
+temperatureMax_difference = tomorrow_temperatureMax - today_temperatureMax
+temperatureMin_difference = tomorrow_temperatureMin - today_temperatureMin
 
-def plus_digit(number):
+def plus_mark(number):
     if number > 0:
         return "+"
     else:
         return ""
 
-tweet = "・明日の最高気温：" + str(tomorrow_temperatureMax) + "℃\n　（前日比" + plus_digit(temperatureMax_difference) + str(temperatureMax_difference) + "℃）\n・明日の最低気温：" + str(tomorrow_temperatureMin) + "℃\n　（前日比" + plus_digit(temperatureMin_difference) + str(temperatureMin_difference) + "℃）\n寒暖差が大きいです。体調に気をつけましょう。"
+tweet = "・明日の最高気温：" + str(tomorrow_temperatureMax) + "℃\n" \
+      + "　（前日比" + plus_mark(temperatureMax_difference) \
+      + str(temperatureMax_difference) + "℃）\n" \
+      + "・明日の最低気温：" + str(tomorrow_temperatureMin) + "℃\n" \
+      + "　（前日比" + plus_mark(temperatureMin_difference) \
+      + str(temperatureMin_difference) + "℃）\n" \
+      + "寒暖差が大きいです。体調に気をつけましょう。"
+
+print(tweet)
 
 #set API key
 consumer_key = os.environ["CONSUMER_KEY_TEMPBOT"]
@@ -44,10 +52,10 @@ auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
 api = tweepy.API(auth_handler=auth)
 
-
 #judge to tweet or not
 def judge_difference():
-    if abs(temperatureMax_difference) >= difference_line  or abs(temperatureMin_difference) >= difference_line:
+    if abs(temperatureMax_difference) >= difference_line \
+    or abs(temperatureMin_difference) >= difference_line:
         return True
     else:
         return False
@@ -62,5 +70,5 @@ def make_tweet():
     else:
         sys.exit()
 
-make_tweet()
+#make_tweet()
 
